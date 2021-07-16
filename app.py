6e38c1c4976bad0f6c46acc8989686cdbc5064cd
sebast825 <sebamolina825@gmail.com>
2021-07-16 19:26:25 -0300

    insercion sin foto

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
 from flaskext.mysql import MySQL
 
 app= Flask(__name__)
@@ -22,6 +22,25 @@ def index():
     conn.commit()
     
     return render_template('empleados/index.html')
+
+@app.route('/create')#PARA QUE AL PONER /CREATE TE REDIRIJA A LA CARPETA CREATE.HTML
+def create():
+   return render_template("empleados/create.html")
+
+@app.route('/store', methods=["POST"])#VIENE CON EL METODO POST
+def storage():
+    _nombre=request.form["txtNombre"]#se le pone el name que esta en create.html
+    _correo=request.form["txtCorreo"]
+    _foto=request.files["txtFoto"]
+
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL,%s,%s,%s);"
+    datos=(_nombre, _correo, _foto.filename)
+
+    conn=mysql.connect()#para conectar a mysql
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
+    return render_template("empleados/index.html")
 
 
 if __name__=='__main__':
